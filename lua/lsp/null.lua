5d1d502a5ccb849ba478bcfb10a733c72b602bcc
neoteny <ss2316544@gmail.com>
2022-01-01 11:55:40 +0200

    updated null-ls config

--- lua/lsp/null.lua
+++ lua/lsp/null.lua
@@ -2,36 +2,21 @@ local M = {}
 
 function M.setup()
   local null_ls = require "null-ls"
-  local fmt = null_ls.builtins.formatting
-  local lint = null_ls.builtins.diagnostics
+  local formatting = null_ls.builtins.formatting
+  local diagnostic = null_ls.builtins.diagnostics
 
   null_ls.setup {
     sources = {
-      -- Lua
-      fmt.stylua,
-      lint.selene,
-
-      -- Golang
-      lint.golangci_lint,
-      fmt.gofumpt,
-      fmt.goimports,
-      -- lint.revive,
-
-      -- JavaScirpt
-      fmt.prettierd,
-      lint.eslint_d,
-
-      -- Shell
-      fmt.shfmt,
-      lint.shellcheck,
-
-      -- Python
-      fmt.black,
-      fmt.isort,
-      lint.flake8,
-
-      -- Rust
-      fmt.rustfmt,
+      formatting.stylua,
+      diagnostic.selene,
+      formatting.gofumpt,
+      formatting.goimports,
+      diagnostic.golangci_lint,
+      formatting.prettierd,
+      diagnostic.eslint_d,
+      formatting.black,
+      formatting.isort,
+      diagnostic.flake8,
     },
   }
 end
