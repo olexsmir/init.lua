1f656feebfed7e20793e4a355f41212aed7aa707
flof-ik <ss2316544@gmail.com>
2022-02-17 19:05:13 +0200

    fixup! update lsp diagnostics

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -6,12 +6,18 @@ function M.setup()
   vim.diagnostic.config(diagnostic)
 
   require("nvim-lsp-installer").on_server_ready(function(server)
+    require("lsp.null").setup()
     local opts = {
       on_attach = on_attach,
       capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
     }
-    require("lsp.null").setup()
 
+    -- set signs
+    for _, sign in ipairs(diagnostic.signs.active) do
+      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
+
+    -- set server config him exists
     local ok, server_opts = pcall(require, "lsp.providers." .. server.name)
     if ok then
       opts = vim.tbl_deep_extend("force", opts, server_opts or {})
