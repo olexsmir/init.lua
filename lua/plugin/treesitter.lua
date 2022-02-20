c81dbfcba3122d57302138f91d8ac85db5e56d3e
flof-ik <ss2316544@gmail.com>
2022-02-20 13:21:19 +0200

    fixup! Added impatient.nvim

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -2,43 +2,11 @@ local M = {}
 
 function M.setup()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "go", "gomod" },
-    highlight = { enable = true },
-    indent = { enable = true, disable = { "python", "yml" } },
-    autotag = { enable = true },
-    context_commentstring = { enable = true },
-    rainbow = {
-      enable = false,
-      extended_mode = true,
-      max_file_lines = 500,
-    },
-    refactor = {
-      highlight_definitions = { enable = true },
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<CR>",
-        scope_incremental = "<CR>",
-        node_incremental = "<TAB>",
-        node_decremental = "<S-TAB>",
-      },
-    },
-    playground = {
-      enable = true,
-      keybindings = {
-        toggle_query_editor = "o",
-        toggle_hl_groups = "i",
-        toggle_injected_languages = "t",
-        toggle_anonymous_nodes = "a",
-        toggle_language_display = "I",
-        focus_language = "f",
-        unfocus_language = "F",
-        update = "R",
-        goto_node = "<cr>",
-        show_help = "?",
-      },
-    },
+    ensure_installed = { "lua", "go", "gomod", "python", "yaml" },
+    indent = { enable = true, disable = { "python", "yaml" } },
+    highlight = { enable = true, additional_vim_regex_highlighting = true },
+    context_commentstring = { enable = true, enable_autocmd = false },
+    autopairs = { enable = true },
   }
 end
 
