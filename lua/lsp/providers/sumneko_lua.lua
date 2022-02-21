90603ee37a94ffd2334f63e3cec8d5e06b9fb133
flof-ik <ss2316544@gmail.com>
2022-02-21 19:50:24 +0200

    Added new globals for sumneko_lua

--- lua/lsp/providers/sumneko_lua.lua
+++ lua/lsp/providers/sumneko_lua.lua
@@ -1,7 +1,18 @@
 return {
   settings = {
     Lua = {
-      diagnostics = { globals = { "vim" } },
+      diagnostics = {
+        globals = {
+          "vim",
+          "describe",
+          "it",
+          "before_each",
+          "after_each",
+          "teardown",
+          "pending",
+          "clear",
+        },
+      },
       workspace = {
         library = {
           [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
