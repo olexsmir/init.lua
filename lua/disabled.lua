5c1e3f5f0e2be9b5f96679106abaa671929e1f5d
flof-ik <ss2316544@gmail.com>
2022-02-24 14:01:54 +0200

    disable perl & ruby plugin support

--- lua/disabled.lua
+++ lua/disabled.lua
@@ -21,6 +21,9 @@ local disabled_built_ins = {
   "matchit",
 }
 
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
 end
