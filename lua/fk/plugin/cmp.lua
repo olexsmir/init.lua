c862958a06d776a5d2ecc484767d9fc0b1de1f29
flof-ik <ss2316544@gmail.com>
2022-03-04 19:24:19 +0200

    cmp: move luasnip to variable

--- lua/fk/plugin/cmp.lua
+++ lua/fk/plugin/cmp.lua
@@ -1,4 +1,5 @@
 local cmp = require "cmp"
+local luasnip = require "luasnip"
 
 -- Snippets
 require("luasnip/loaders/from_vscode").load {
@@ -14,7 +15,7 @@ cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm
 cmp.setup {
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   formatting = {
@@ -61,7 +62,7 @@ cmp.setup {
     ["<Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
+      elseif luasnip.expand_or_jumpable() then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
         fallback()
@@ -70,7 +71,7 @@ cmp.setup {
     ["<S-Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
+      elseif luasnip.jumpable(-1) then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
       else
         fallback()
