325ec906deaa9c5f1d1f1f3c89efc8c87a9a4bbd
flof-ik <ss2316544@gmail.com>
2022-03-09 13:04:40 +0200

    update lsp config

--- lua/fk/lsp/init.lua
+++ lua/fk/lsp/init.lua
@@ -14,6 +14,7 @@ function M.setup()
     local opts = {
       on_attach = on_attach,
       capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      flags = { debounce_text_changes = 150 },
     }
 
     -- setup server config if it exists
