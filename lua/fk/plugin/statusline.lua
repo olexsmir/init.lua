d3527fafb1f48ce994a02bec942e930995c1f0e3
flof-ik <ss2316544@gmail.com>
2022-03-09 14:56:40 +0200

    Updated statusline config

--- lua/fk/plugin/statusline.lua
+++ lua/fk/plugin/statusline.lua
@@ -8,10 +8,18 @@ require("lualine").setup {
   },
   sections = {
     lualine_a = { "filename" },
-    lualine_b = { "branch", "diff" },
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = { { "diagnostics", sources = { "nvim_diagnostic" } } },
-    lualine_z = { "location" },
+    lualine_b = { "branch" },
+    lualine_c = { "location" },
+    lualine_x = { "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
   },
 }
