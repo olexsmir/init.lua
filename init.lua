c24e78d9f48af748cff7a80361dda5e927a10971
flof-ik <ss2316544@gmail.com>
2022-03-09 22:56:28 +0200

    updated init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
 require "impatient"
 require "fk.options"
-require "fk.plugin"
-require "fk.keymapings"
 require "fk.disabled"
 require "fk.globals"
 require "fk.autocmd"
+
+vim.defer_fn(function()
+  require "fk.plugin"
+end, 0)
+
+require "fk.keymapings"
 require "fk.theme"
