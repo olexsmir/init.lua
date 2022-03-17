46819581161198d432a7e3987c6395667f1fa3bc
flof-ik <ss2316544@gmail.com>
2022-03-17 13:35:43 +0200

    fixup! Updated keymaps

---
+++ ftplugin/rust.lua
@@ -0,0 +1,19 @@
+-- spacing
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+
+require("which-key").register({
+  [";"] = {
+    name = "Coding",
+    t = { "<cmd>RustToggleInlayHints<cr>", "Toggle type hints" },
+    r = { "<cmd>RustRunnables<cr>", "Run" },
+    R = { "<cmd>RustReloadWorkspace<cr>", "Reload Workspace" },
+    K = { "<cmd>RustMoveItemUp<cr>", "Move current item up" },
+    J = { "<cmd>RustMoveItemDown<cr>", "Move current item down" },
+  },
+}, {
+  prefix = "<leader>",
+  mode = "n",
+  { noremap = true, silent = true },
+})
