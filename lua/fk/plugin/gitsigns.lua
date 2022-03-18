5f09ce93d163ef64e3828c1ab18e18e1bb4a6da9
flof-ik <ss2316544@gmail.com>
2022-03-18 12:53:44 +0200

    updated gitsigns config

--- lua/fk/plugin/gitsigns.lua
+++ lua/fk/plugin/gitsigns.lua
@@ -1,36 +1,9 @@
 require("gitsigns").setup {
-  signs = {
-    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-  },
-  signcolumn = true,
-  numhl = false,
-  linehl = false,
-  word_diff = false,
-  watch_gitdir = {
-    interval = 1000,
-    follow_files = true,
-  },
+  max_file_length = 1000,
   attach_to_untracked = true,
   current_line_blame = true,
   current_line_blame_opts = {
-    virt_text = true,
     virt_text_pos = "eol",
     delay = 300,
-  },
-  current_line_blame_formatter_opts = { relative_time = false },
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil,
-  max_file_length = 1500,
-  preview_config = {
-    border = "single",
-    style = "minimal",
-    relative = "cursor",
-    row = 0,
-    col = 1,
   },
 }
