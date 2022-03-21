79157c34480ee348718908a686f9b69a5bbf7e71
Smirnov Alexander <ss2316544@gmail.com>
2022-03-21 22:43:45 +0200

    disable node plugin provider

--- lua/fk/disabled.lua
+++ lua/fk/disabled.lua
@@ -23,6 +23,7 @@ local disabled_built_ins = {
 
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
 
 for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
