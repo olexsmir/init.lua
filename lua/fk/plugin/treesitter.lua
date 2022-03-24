4aaee5f054712f671a55740a6ff5323478f07f4c
Smirnov Alexander <ss2316544@gmail.com>
2022-03-24 22:05:02 +0200

    updated treesitter config

--- lua/fk/plugin/treesitter.lua
+++ lua/fk/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = { "lua", "go", "gomod", "python", "yaml" },
   indent = { enable = true, disable = { "python", "go", "yaml" } },
-  highlight = { enable = true, additional_vim_regex_highlighting = true },
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
   context_commentstring = { enable = true, enable_autocmd = false },
   autopairs = { enable = true },
 }
