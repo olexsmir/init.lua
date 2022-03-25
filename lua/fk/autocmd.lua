c168ffee15d3e3aabf9b1237f90e221a26c4d663
Smirnov Alexander <ss2316544@gmail.com>
2022-03-25 21:06:18 +0200

    disable auto-create directory if it not found

--- lua/fk/autocmd.lua
+++ lua/fk/autocmd.lua
@@ -1,2 +1,1 @@
-vim.cmd [[ autocmd BufWritePre * lua mkdir() ]]
 vim.cmd [[ autocmd TextYankPost * lua vim.highlight.on_yank() ]]
--- lua/fk/globals.lua
+++ lua/fk/globals.lua
@@ -1,11 +1,4 @@
 -- selene: allow(global_usage)
-function _G.mkdir()
-  local dir = vim.fn.expand "%:p:h"
-  if vim.fn.isdirectory(dir) == 0 then
-    vim.fn.mkdir(dir, "p")
-  end
-end
-
 function _G.P(v)
   print(vim.inspect(v))
   return v
