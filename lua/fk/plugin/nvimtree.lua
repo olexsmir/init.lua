7267f43315a49264d03f07de19979b30492792c0
Smirnov Alexander <ss2316544@gmail.com>
2022-03-25 21:17:47 +0200

    updated nvimtree config

--- lua/fk/plugin/nvimtree.lua
+++ lua/fk/plugin/nvimtree.lua
@@ -2,9 +2,7 @@ vim.g.nvim_tree_group_empty = 1
 
 require("nvim-tree").setup {
   disable_netrw = true,
-  hijack_netrw = true,
   auto_close = true,
-  auto_open = true,
   update_cwd = true,
   diagnostics = {
     enable = true,
@@ -19,26 +17,14 @@ require("nvim-tree").setup {
     dotfiles = true,
     custom = { ".git", "node_modules", "__pycache__", "vendor", "env", ".bin" },
   },
-  git = {
-    enable = true,
-    ignore = false,
-    timeout = 300,
-  },
+  git = { enable = true, ignore = false },
   view = {
-    width = 30,
-    auto_resize = false,
-    hide_root_folder = false,
     mappings = {
-      custom_only = false,
       list = {
         { key = { "l", "<CR>", "o" }, action = "edit" },
         { key = "h", action = "close_node" },
         { key = "v", action = "vsplit" },
       },
     },
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
   },
 }
