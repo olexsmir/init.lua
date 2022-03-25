d818ee85b1ece8ed42a1b8a9a22ebe02e721546b
Smirnov Alexander <ss2316544@gmail.com>
2022-03-25 22:05:16 +0200

    updated lsp opts config setup

--- lua/fk/lsp/init.lua
+++ lua/fk/lsp/init.lua
@@ -4,13 +4,11 @@ function M.setup()
   require "fk.lsp.diagnostic"()
   require "fk.lsp.null"
   require("nvim-lsp-installer").on_server_ready(function(server)
-    local opts = {
+    local opts = require "fk.lsp.providers"(server, {
       on_attach = require "fk.lsp.attach",
       capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
       flags = { debounce_text_changes = 150 },
-    }
-
-    opts = require "fk.lsp.providers"(server, opts)
+    })
 
     server:setup(opts)
     vim.cmd [[ do User LspAttachBuffers ]]
