23fdee800b84042dc3dc3ccf3d3b7f54dc912029
Smirnov Alexander <ss2316544@gmail.com>
2022-03-26 19:09:35 +0200

    refactor(lsp): setup function

--- lua/fk/lsp/init.lua
+++ lua/fk/lsp/init.lua
@@ -4,13 +4,11 @@ function M.setup()
   require "fk.lsp.diagnostic"()
   require "fk.lsp.null"
   require("nvim-lsp-installer").on_server_ready(function(server)
-    local opts = require "fk.lsp.providers"(server, {
+    server:setup(require "fk.lsp.providers"(server, {
       on_attach = require "fk.lsp.attach",
       capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
       flags = { debounce_text_changes = 150 },
-    })
-
-    server:setup(opts)
+    }))
     vim.cmd [[ do User LspAttachBuffers ]]
   end)
 end
