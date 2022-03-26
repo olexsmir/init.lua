b7cc248a01636f6aa6eb25190b6c1e9bf5c24cfc
Smirnov Alexander <ss2316544@gmail.com>
2022-03-26 12:50:47 +0200

    added pyproject.toml in project.nvim patterns

--- lua/fk/plugin/project.lua
+++ lua/fk/plugin/project.lua
@@ -1,7 +1,7 @@
 require("project_nvim").setup {
   manual_mode = false,
   detection_methods = { "lsp", "pattern" },
-  patterns = { ".git", "Makefile", "package.json", "go.mod" },
+  patterns = { ".git", "Makefile", "package.json", "go.mod", "pyproject.toml" },
   show_hidden = false,
   silent_chdir = true,
 }
