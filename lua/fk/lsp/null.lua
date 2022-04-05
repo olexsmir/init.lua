ac2e99e13b5fa06ad6ade846f0618fafed041bd5
Smirnov Olexander <ss2316544@gmail.com>
2022-04-05 17:33:59 +0300

    null-ls removed php and markdown roematers

--- lua/fk/lsp/null.lua
+++ lua/fk/lsp/null.lua
@@ -1,9 +1,12 @@
 local null_ls = require "null-ls"
 local formatting = null_ls.builtins.formatting
 local diagnostic = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup {
   sources = {
+    code_actions.gitrebase,
+
     formatting.stylua,
     diagnostic.selene,
     formatting.gofumpt,
@@ -12,7 +15,5 @@ null_ls.setup {
     formatting.black,
     formatting.isort,
     diagnostic.flake8,
-    formatting.markdownlint,
-    formatting.phpcbf,
   },
 }
