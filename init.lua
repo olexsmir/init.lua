94a3eb70748f0b45d65d14167c678def7e85e54a
Smirnov Olexander <ss2316544@gmail.com>
2022-04-14 14:02:03 +0300

    move autocmd' to init.lua

--- init.lua
+++ init.lua
@@ -2,8 +2,9 @@ require "fk.options"
 require "fk.keymapings"
 require "fk.disable"
 require "fk.globals"
-require "fk.autocmd"
 require "fk.plugin"
+
+vim.cmd [[ autocmd TextYankPost * lua vim.highlight.on_yank() ]]
 
 require("tokyonight").colorscheme()
 require("impatient").enable_profile()
--- lua/fk/autocmd.lua
+++
@@ -1,1 +0,0 @@
-vim.cmd [[ autocmd TextYankPost * lua vim.highlight.on_yank() ]]
