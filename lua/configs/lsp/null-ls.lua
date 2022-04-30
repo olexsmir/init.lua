c861ea2cd6deefad685ad5ff664704a9e9daae39
Smirnov Olexander <ss2316544@gmail.com>
2022-04-30 23:48:24 +0300

    reformat lua code by stylua

--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -4,8 +4,8 @@ local formatting = null_ls.builtins.formatting
 local diagnostic = null_ls.builtins.diagnostics
 
 null_ls.setup {
-    sources = {
-        formatting.stylua,
-        diagnostic.selene,
-    }
+  sources = {
+    formatting.stylua,
+    diagnostic.selene,
+  },
 }
