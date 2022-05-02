01ec633dfe95c48f20afe8147ebf784013e54ff0
Smirnov Olexander <ss2316544@gmail.com>
2022-05-02 14:45:24 +0300

    feat(autocmd): added file specific settings

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,8 +1,47 @@
-local cmd = vim.api.nvim_create_autocmd
+local cmd, opt = vim.api.nvim_create_autocmd, vim.opt_local
 
 cmd("TextYankPost", {
   pattern = "*",
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+cmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    opt.tabstop = 2
+    opt.shiftwidth = 2
+    opt.softtabstop = 2
+
+    require("core.utils").map("n", "<leader>;t", "<Plug>PlenaryTestFile")
+  end,
+})
+
+cmd("FileType", {
+  pattern = "go",
+  callback = function()
+    opt.expandtab = false
+    opt.tabstop = 4
+    opt.shiftwidth = 4
+    opt.softtabstop = 4
+  end,
+})
+
+cmd("FileType", {
+  pattern = "python",
+  callback = function()
+    opt.tabstop = 4
+    opt.shiftwidth = 4
+    opt.softtabstop = 4
+  end,
+})
+
+cmd("FileType", {
+  pattern = "javascript,typescript",
+  callback = function()
+    opt.tabstop = 2
+    opt.shiftwidth = 2
+    opt.softtabstop = 2
   end,
 })
