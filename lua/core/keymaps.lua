da57e8c1647b0f52b43518220414aa7420e48da7
Smirnov Olexander <ss2316544@gmail.com>
2022-05-10 13:21:24 +0300

    feat(keymaps): map `PackerSync`

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -36,6 +36,7 @@ map("n", "<A-k>", ":m .-2<CR>==")
 
 -- plugins
 map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
+map("n", "<leader>ps", "<cmd>PackerSync<cr>")
 
 -- telescope
 map("n", "<leader>f", "<cmd>Telescope find_files<cr>")
