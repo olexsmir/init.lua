7c3dc2603116a4ee5fbb20cb207a5f9ad90edfd6
Smirnov Oleksandr <ss2316544@gmail.com>
2022-07-03 00:08:47 +0300

    feat(neogit): disable commit confirming

--- lua/configs/neogit.lua
+++ lua/configs/neogit.lua
@@ -1,6 +1,7 @@
 require("neogit").setup {
   kind = "vsplit",
   disable_builtin_notifications = true,
+  disable_commit_confirmation = true,
   disable_hint = true,
   signs = {
     section = { "", "" },
