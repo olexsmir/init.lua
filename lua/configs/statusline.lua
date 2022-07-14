0d21e0095dc76187cf81e8823511ddee9f217620
Smirnov Oleksandr <ss2316544@gmail.com>
2022-07-15 00:39:43 +0300

    feat(lualine): update config

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -1,3 +1,32 @@
+local c = {
+  mode = {
+    function()
+      return " "
+    end,
+    padding = 0,
+  },
+  diagnostic = { "diagnostics", sources = { "nvim_diagnostic" } },
+  location = { "location", padding = 0, colored = false },
+  lsp = {
+    function()
+      local clients = vim.lsp.get_active_clients { bufnr = 0 }
+      local client_names = {}
+      for _, client in pairs(clients) do
+        if client.name ~= "null-ls" then
+          table.insert(client_names, client.name)
+        end
+      end
+
+      local client_names_str = table.concat(client_names, ", ")
+      if #client_names_str == 0 then
+        return ""
+      else
+        return "[" .. client_names_str .. "]"
+      end
+    end,
+  },
+}
+
 require("lualine").setup {
   options = {
     theme = "kanagawa",
@@ -12,11 +41,11 @@ require("lualine").setup {
     },
   },
   sections = {
-    lualine_a = { "filename" },
-    lualine_b = { "branch" },
-    lualine_c = { "location" },
-    lualine_x = { "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
+    lualine_a = { c.mode },
+    lualine_b = { "filename" },
+    lualine_c = { "branch", c.diagnostic },
+    lualine_x = { c.lsp, "diff" },
     lualine_y = {},
-    lualine_z = {},
+    lualine_z = { c.location },
   },
 }
