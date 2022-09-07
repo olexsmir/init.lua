1d6e97ffd2db571fcfbef62cc2d83800bf589a08
Smirnov Oleksandr <ss2316544@gmail.com>
2022-09-07 19:02:22 +0300

    refactor(nvimtree): update config

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,13 +1,10 @@
 require("nvim-tree").setup {
-  update_cwd = true,
   diagnostics = { enable = true },
+  renderer = { group_empty = true },
+  git = { enable = true },
   filters = {
-    dotfiles = true,
     custom = { "^\\.git$", "node_modules", "__pycache__", "vendor", "target" },
   },
-  renderer = { group_empty = true },
-  git = { enable = true, ignore = false },
-  log = { enable = false },
   view = {
     side = "right",
     mappings = {
