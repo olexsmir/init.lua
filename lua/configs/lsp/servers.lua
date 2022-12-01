fc37594140d4439a0e437dbe0423b7f4eb15e184
Smirnov Oleksandr <ss2316544@gmail.com>
2022-12-01 14:28:40 +0200

    feat(lsp): setup denols and tsserver

--- lua/configs/lsp/servers.lua
+++ lua/configs/lsp/servers.lua
@@ -1,5 +1,8 @@
+local lspconfig = require "lspconfig"
+
 return {
-  tsserver = {},
+  denols = { root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc") },
+  tsserver = { root_dir = lspconfig.util.root_pattern "package.json" },
   dockerls = {},
   gopls = {
     settings = {
