a9c77eff8410990f85902885a58cda5dc94fdcb6
Smirnov Oleksandr <ss2316544@gmail.com>
2022-12-26 18:56:23 +0200

    feat(ftplugin): update keymaps

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,10 @@
+local map = require("core.utils").map
+
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
+
+-- some maps
+map("n", "<leader>;s", function()
+  vim.cmd.source "%"
+end)
