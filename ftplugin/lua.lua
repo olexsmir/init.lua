332b807bb20b75c8d5f0c40801b1ec05d8e91730
Smirnov Oleksandr <ss2316544@gmail.com>
2023-01-15 20:32:42 +0200

    lua: add keymap for running current file tests

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -5,6 +5,7 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 
 -- some maps
+map("n", "<leader>;t", "<cmd>PlenaryBustedFile %<cr>")
 map("n", "<leader>;s", function()
   vim.cmd.source "%"
 end)
