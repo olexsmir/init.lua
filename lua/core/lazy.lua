949acaeaea5e88fbd18db4a7e5f90dd955166a52
Smirnov Oleksandr <ss2316544@gmail.com>
2023-01-28 13:11:24 +0200

    disable buildins w/ lazy.nvim

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -13,4 +13,30 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
   defaults = { lazy = true },
   dev = { path = "~/code" },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "gzip",
+        "zip",
+        "tohtml",
+        "man",
+        "zipPlugin",
+        "tar",
+        "tarPlugin",
+        "getscript",
+        "getscriptPlugin",
+        "vimball",
+        "vimballPlugin",
+        "2html_plugin",
+        "logipat",
+        "rrhelper",
+        "spellfile_plugin",
+        "matchit",
+      },
+    },
+  },
 })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -51,27 +51,3 @@ vim.o.writebackup = false
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
-for _, i in pairs {
-  "netrw",
-  "netrwPlugin",
-  "netrwSettings",
-  "netrwFileHandlers",
-  "gzip",
-  "zip",
-  "tohtml",
-  "man",
-  "zipPlugin",
-  "tar",
-  "tarPlugin",
-  "getscript",
-  "getscriptPlugin",
-  "vimball",
-  "vimballPlugin",
-  "2html_plugin",
-  "logipat",
-  "rrhelper",
-  "spellfile_plugin",
-  "matchit",
-} do
-  vim.g["loaded_" .. i] = 1
-end
