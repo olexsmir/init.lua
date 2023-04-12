9e00a04cede57d4da1ee0278971743928b0b1240
Smirnov Oleksandr <ss2316544@gmail.com>
2023-04-12 15:53:48 +0300

    feat(plugins): add harpoon and update plugins

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,13 @@
+local map = require("core.utils").smap
+return {
+  "ThePrimeagen/harpoon",
+  config = true,
+  keys = {
+    { "<leader>a", map("harpoon.mark", "add_file") },
+    { "<leader>1", map("harpoon.ui", "toggle_quick_menu") },
+    { "<leader>2", map("harpoon.ui", "nav_file", "1") },
+    { "<leader>3", map("harpoon.ui", "nav_file", "2") },
+    { "<leader>4", map("harpoon.ui", "nav_file", "3") },
+    { "<leader>5", map("harpoon.ui", "nav_file", "4") },
+  },
+}
