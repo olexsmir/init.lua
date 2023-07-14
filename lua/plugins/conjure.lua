4b1dc768c34bd266a368b4713c1c247f4e7a06d3
Smirnov Oleksandr <ss2316544@gmail.com>
2023-07-15 01:08:22 +0300

    feat(conjure): change file name, and refactor config

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -7,11 +7,14 @@ return {
     "PaterJason/cmp-conjure",
     { "eraserhd/parinfer-rust", build = "cargo build --release" },
   },
-  config = function()
+  init = function()
     vim.g["conjure#mapping#doc_word"] = false
     vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
     vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = true
-
+  end,
+  config = function()
+    require("conjure.main").main()
+    require("conjure.mapping")["on-filetype"]()
     require("cmp").setup.filetype(ft, {
       sources = { { name = "conjure", max_item_count = 4 } },
     })
