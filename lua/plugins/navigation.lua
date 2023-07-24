fe0753ec1bb3794daf56e88aa132341713e7e875
Smirnov Oleksandr <ss2316544@gmail.com>
2023-07-24 23:38:32 +0300

    feat(harpoon): chage keymaps

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -7,10 +7,10 @@ return {
     keys = {
       { "<leader>a", map("harpoon.mark", "add_file") },
       { "<leader>1", map("harpoon.ui", "toggle_quick_menu") },
-      { "<leader>3", map("harpoon.ui", "nav_file", "1") },
-      { "<leader>4", map("harpoon.ui", "nav_file", "2") },
-      { "<leader>5", map("harpoon.ui", "nav_file", "3") },
-      { "<leader>6", map("harpoon.ui", "nav_file", "4") },
+      { "<leader>2", map("harpoon.ui", "nav_file", "1") },
+      { "<leader>3", map("harpoon.ui", "nav_file", "2") },
+      { "<leader>4", map("harpoon.ui", "nav_file", "3") },
+      { "<leader>5", map("harpoon.ui", "nav_file", "4") },
     },
   },
 
