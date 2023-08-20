c08ca057655c4904ab03833198b67a3e8987a3bd
Smirnov Oleksandr <ss2316544@gmail.com>
2023-08-21 00:56:28 +0300

    feat(mini): update mini.indentscope

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,5 +19,21 @@ return {
         end,
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "gitcommit",
+          "NeogitCommitMessage",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
   },
 }
