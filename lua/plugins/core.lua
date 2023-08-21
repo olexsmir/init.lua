397c472935ff43e5a7360f8d6d1f2aedbb6bf33c
Smirnov Oleksandr <ss2316544@gmail.com>
2023-08-22 01:29:14 +0300

    refactor: store core plugins in core

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -18,4 +18,41 @@ return {
     event = "BufRead",
     config = true,
   },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+      disable_filetype = { "TelescopePrompt", "markdown" },
+    },
+  },
+  {
+    "numToStr/Comment.nvim",
+    event = "BufRead",
+    keys = "gc",
+    config = true,
+  },
+  {
+    "ahmedkhalf/project.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("project_nvim").setup {
+        show_hidden = true,
+        patterns = {
+          "^\\.git$",
+          "Makefile",
+          "Justfile",
+          [[Taskfile\.ya?ml]],
+          [[docker-compose\.ya?ml]],
+          "*.Dockerfile.*",
+          "package.json",
+          "go.mod",
+          "go.work",
+          "pyproject.toml",
+          "Cargo.toml",
+          "deps.edn",
+        },
+      }
+    end,
+  },
 }
