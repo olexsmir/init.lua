4a03f6db344fad286c07326e12a20a3cb6956720
Smirnov Oleksandr <ss2316544@gmail.com>
2023-08-22 01:44:01 +0300

    clean up completions and git config

--- lua/plugins/lsp/attach.lua
+++ lua/plugins/lsp/attach.lua
@@ -18,12 +18,9 @@ return {
     end
 
     map("gd", "<cmd>Telescope lsp_definitions<cr>")
-    map("gD", vim.lsp.buf.declaration)
     map("gr", "<cmd>Telescope lsp_references<cr>")
     map("gi", "<cmd>Telescope lsp_implementations<cr>")
-    map("gs", vim.lsp.buf.signature_help)
     map("gl", vim.diagnostic.open_float)
-    map("<leader>li", "<cmd>LspInfo<cr>")
     map("<leader>la", vim.lsp.buf.code_action)
     map("<leader>lr", vim.lsp.buf.rename)
     map("<leader>lf", "<cmd>lua vim.lsp.buf.format {async = true}<cr>")
@@ -33,7 +30,6 @@ return {
     map("[d", vim.diagnostic.goto_prev)
   end,
   null_ls = function(_, _)
-    map("<leader>li", "<cmd>LspInfo<cr>")
     map("<leader>lf", "<cmd>lua vim.lsp.buf.format {async = true}<cr>")
     map("]d", vim.diagnostic.goto_next)
     map("[d", vim.diagnostic.goto_prev)
