ab3151a2284f9bbdc2478dad5ce45225842166db
Smirnov Oleksandr <ss2316544@gmail.com>
2024-01-11 03:55:40 +0200

    feat(telescope): lazy load

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
+  event = "VeryLazy",
   dependencies = { "nvim-telescope/telescope-ui-select.nvim" },
   keys = {
     { "<leader>f", "<cmd>Telescope find_files<cr>" },
