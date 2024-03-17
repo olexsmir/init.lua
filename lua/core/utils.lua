b2e97aa8fc63d2fa7d0a6e5077bb219160f99f93
Smirnov Oleksandr <ss2316544@gmail.com>
2024-03-17 23:46:39 +0200

    refactor(utils): remove quickfix toggler

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,4 +1,3 @@
-local is_qf_exists = false
 return {
   ---@param mode string|table
   ---@param from string
@@ -10,25 +9,5 @@ return {
       silent = true,
       buffer = buffer or false,
     })
-  end,
-
-  qf_toggle = function()
-    is_qf_exists = false
-    for _, win in pairs(vim.fn.getwininfo()) do
-      is_qf_exists = win["quickfix"] == 1
-    end
-
-    if is_qf_exists == true then
-      return vim.cmd.cclose()
-    end
-
-    if not vim.tbl_isempty(vim.fn.getqflist()) then
-      vim.cmd.copen()
-    end
-  end,
-
-  ---@return boolean
-  get_qf_status = function()
-    return is_qf_exists
   end,
 }
