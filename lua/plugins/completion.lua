fd2cc56902b6acc253c48a60996c1710c6a48ab4
Smirnov Oleksandr <ss2316544@gmail.com>
2024-03-29 22:31:52 +0200

    fix(cmp): it helps but not for gopls

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -67,7 +67,7 @@ return {
           ["<C-d>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete {},
           ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
+            behavior = cmp.ConfirmBehavior.Insert,
             select = false,
           },
           ["<Tab>"] = function(fallback)
