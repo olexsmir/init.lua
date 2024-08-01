24820383f9bbe45941325a6eaf4e3092676f639b
Smirnov Oleksandr <ss2316544@gmail.com>
2024-08-01 21:09:44 +0300

    refactor(lsp): update config of lazydev

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -16,7 +16,8 @@ return {
       opts = {
         library = {
           { path = "luvit-meta/library", words = { "vim%.uv" } },
-          { path = "lazy.nvim" },
+          "lazy.nvim",
+          "gopher.nvim",
         },
       },
     },
