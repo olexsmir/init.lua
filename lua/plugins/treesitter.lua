56836bc61e7075896949cc08ad003ac7385c9385
Smirnov Oleksandr <ss2316544@gmail.com>
2024-08-04 14:40:23 +0300

    feat(treesitter): install headlines to change the background color of sql code in go

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,23 @@ return {
   dependencies = {
     "RRethy/nvim-treesitter-endwise",
     { "nvim-treesitter/nvim-treesitter-context", config = true },
+    {
+      "lukas-reineke/headlines.nvim",
+      config = function()
+        require("headlines").setup {
+          go = {
+            codeblock_highlight = "ColorColumn",
+            query = vim.treesitter.query.parse(
+              "go",
+              [[([(raw_string_literal)
+                  (interpreted_string_literal)] @codeblock
+                (#match? @codeblock "--sql" "-- sql" "--SQL" "-- SQL")
+                (#offset! @codeblock 0 0 1 0))]]
+            ),
+          },
+        }
+      end,
+    },
   },
   build = function()
     pcall(vim.cmd.TSUpdate)
@@ -13,7 +30,7 @@ return {
   ---@diagnostic disable-next-line: missing-fields
   opts = {
     auto_install = true,
-    ensure_installed = { "sql", "markdown", "markdown_inline", "vim" },
+    ensure_installed = { "sql", "markdown", "markdown_inline", "vim", "query" },
     indent = { enable = true, disable = { "python" } },
     highlight = { enable = true, additional_vim_regex_highlighting = false },
     autopairs = { enable = true },
