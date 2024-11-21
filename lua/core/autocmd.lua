fcf0e72dcfcefc4bd869030e9b61b605dc35a3e8
Smirnov Oleksandr <ss2316544@gmail.com>
2024-11-21 20:07:18 +0200

    refactor(options): autocmds belogs to other autocmds

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -29,3 +29,21 @@ u.aucmd("FileType", {
     u.map("n", "<C-p>", "<cmd>cprev<CR>", true)
   end,
 })
+
+u.aucmd("FileType", {
+  group = u.augroup "formatoptions",
+  callback = function()
+    vim.opt.formatoptions:remove {
+      "c", -- autowrap comments using textwidth with leader
+      "r", -- don't auto-insert comment leader on enter in insert
+      "o", -- don't auto-insert comment leader on o/O in normal
+      "n", -- don't recognized numbered lists
+      "2", -- don't use the indent of second paragraph line
+    }
+
+    vim.opt.formatoptions:append {
+      "l", -- long lines not broken in insert mode
+      "1", -- don't break a line after a one-letter word
+    }
+  end,
+})
