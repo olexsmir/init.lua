f10f6ec19f6f44bd0689aa5f72aa85a755f474d4
Oleksandr Smirnov <olexsmir@gmail.com>
2025-02-14 15:17:15 +0200

    fix(ftplugin): fix keymaps

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -5,7 +5,7 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 
 -- some maps
-map("n", "<localleader>;t", ":=MiniTest.run_file()<CR>", true)
+map("n", "<localleader>t", ":=MiniTest.run_file()<CR>", true)
 map("n", "<localleader>x", function()
   vim.cmd.source "%"
 end, true)
