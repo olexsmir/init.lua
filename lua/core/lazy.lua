57808ced0341f2c3620177e1b06bf2c0bd059cc2
Oleksandr Smirnov <olexsmir@gmail.com>
2025-03-30 02:55:03 +0200

    update my hidden stuff

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -15,6 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
   spec = {
     { import = "plugins" },
+    { require("hidden").plugins() },
   },
   defaults = {
     lazy = true,
--- lua/hidden/init.lua
+++ lua/hidden/init.lua
@@ -1,15 +1,28 @@
----@param m string
----@return unknown
--- stylua: ignore
-local function sreq(m)
-  local ok, r = pcall(require, m)
-  if not ok then return nil end
+--- this is a place where i put all my hidden stuff,
+--- like things that i just feel like private to me
+--- of things that related to my work
+--- also probably i in the future i might delete this file all together
+
+local hidden = {}
+local function wrap(mod)
+  local ok, r = pcall(require, mod)
+  if not ok then
+    error(mod .. " not found")
+  end
   return r
 end
 
-local M = {}
+---@class hidden.org
+---@field weekly_template string
+---@field daily_template string
+hidden.org = wrap "hidden.org"
 
----@type hidden.org
-M.org = sreq "hidden.org"
+hidden.plugins = function()
+  local ok, plugins = pcall(require, "hidden.plugins")
+  if not ok then
+    return {}
+  end
+  return plugins
+end
 
-return M
+return hidden
