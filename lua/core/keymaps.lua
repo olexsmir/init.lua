d2bf52956a319867a8386fdf16c2b1e337707a70
Oleksandr Smirnov <olexsmir@gmail.com>
2025-04-10 23:53:36 +0300

    refactor(keymaps): now that keys are mapped by default

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,8 +19,6 @@ u.map("i", "jk", "<esc>")
 u.map("n", "<leader>h", "<cmd>nohlsearch<cr>")
 u.map("n", "<C-d>", "<C-d>zz")
 u.map("n", "<C-u>", "<C-u>zz")
-u.map("n", "[b", ":bp<cr>")
-u.map("n", "]b", ":bn<cr>")
 
 -- macros
 u.map("n", "Q", "@qj")
