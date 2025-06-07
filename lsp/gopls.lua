71747fc1581e384e35db6aeec2a7fcb8711ff628
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-08 02:17:25 +0300

    feat(gopsl): update the analyses list

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -15,9 +15,16 @@ return {
         unusedparams = true,
         unreachable = true,
         unusedwrite = true,
-        useany = true,
         shadow = true,
-        nilness = true,
+        S1008 = true, -- simplify returning boolean expression
+        SA5000 = true, -- assignment to nil map
+        SA5007 = true, -- infinite recursion call
+        ST1019 = true, -- importing the same package multiple times
+        SA1000 = true, -- invalid regular expression
+        SA1020 = true, -- using an invalid host:port pair with a net.Listen-related function
+        SA1023 = true, -- modifying the buffer in an io.Writer implementation
+        SA9001 = true, -- defers in range loops may not run when you expect them to
+        ST1013 = true, -- should use constants for HTTP error codes, not magic numbers
       },
       hints = {
         assignVariableTypes = true,
