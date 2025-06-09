b8afdd90918ac5cc0e33ac84f7e26ce77cc0c64e
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-09 20:17:10 +0300

    refactor(treesitter): fix injections

--- after/queries/go/injections.scm
+++ after/queries/go/injections.scm
@@ -8,10 +8,12 @@
    (argument_list [(interpreted_string_literal)
                    (raw_string_literal)] @injection.content))
  (#offset! @injection.content 0 1 0 -1)
- (#set! injection.language "sql"))
+ (#set! injection.include-children)
+ (#set! injection.language sql))
 
 ([(raw_string_literal)
   (interpreted_string_literal)] @injection.content
- (#match? @injection.content "--sql" "-- sql" "--SQL" "-- SQL" )
+ (#match? @injection.content "--sql" "-- sql" "--SQL" "-- SQL")
  (#offset! @injection.content 0 1 0 -1)
- (#set! injection.language "sql"))
+ (#set! injection.include-children)
+ (#set! injection.language sql))
