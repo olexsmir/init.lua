371ab0f506ee330a940fa45d607753744dffc73e
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-27 17:22:44 +0300

    refactor(ftplugin): remove org, use consistent keymaps for local keymaps

--- after/ftplugin/org.lua
+++
@@ -1,3 +0,0 @@
-vim.opt_local.wrap = true
-vim.opt_local.conceallevel = 2
-vim.opt_local.concealcursor = "cv"
--- after/ftplugin/todotxt.lua
+++ after/ftplugin/todotxt.lua
@@ -5,13 +5,13 @@ local function get_date()
 end
 
 -- add
-map("n", "<leader>;a", function()
+map("n", "<localleader>a", function()
   vim.cmd("put ='" .. get_date() .. " '")
   vim.api.nvim_feedkeys("A", "n", false)
   vim.cmd "normal! a"
 end)
 
-map("n", "<leader>;d", function()
+map("n", "<localleader>d", function()
   local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
   local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
   vim.api.nvim_buf_set_lines(
