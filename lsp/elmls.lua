fd62565830caaf2b91a50c855a8803245bb16d82
Oleksandr Smirnov <olexsmir@gmail.com>
2025-09-07 20:46:41 +0300

    refactor(lsp): minify elmls config

--- lsp/elmls.lua
+++ lsp/elmls.lua
@@ -4,17 +4,7 @@ local u = require("core.utils").lsp
 return {
   cmd = { "elm-language-server" },
   filetypes = { "elm" },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
-    if
-      filetype == "elm" or (filetype == "json" and fname:match "elm%.json$")
-    then
-      on_dir(vim.fs.root(fname, "elm.json"))
-      return
-    end
-    on_dir(nil)
-  end,
+  root_markers = { "elm.json" },
   init_options = {
     elmReviewDiagnostics = "warning",
     skipInstallPackageConfirmation = false,
