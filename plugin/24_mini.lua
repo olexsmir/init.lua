74dcf99ca448f7e10bba9a4692765ce00af09ffd
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-14 14:55:48 +0200

    now we're friends with mini.files

--- plugin/24_mini.lua
+++ plugin/24_mini.lua
@@ -1,5 +1,9 @@
 Config.add "echasnovski/mini.nvim"
 
+Config.map("n", "<leader>e", function()
+  MiniFiles.open(vim.api.nvim_buf_get_name(0), true)
+  MiniFiles.reveal_cwd()
+end)
 Config.map("n", "<leader>c", function()
   require("mini.bufremove").delete()
 end)
@@ -14,3 +18,31 @@ Config.later(function()
   require("mini.splitjoin").setup {}
   require("mini.test").setup {}
 end)
+
+Config.aucmd2("User", "MiniFilesActionRename", function(ev)
+  Snacks.rename.on_rename_file(ev.data.from, ev.data.to)
+end)
+
+require("mini.files").setup {
+  content = {
+    filter = function(f)
+      return ({
+        [".bin"] = {},
+        [".docker"] = {},
+        [".git"] = {},
+        [".jj"] = {},
+        ["build"] = {},
+        ["dist"] = {},
+        ["elm-stuff"] = {},
+        ["node_modules"] = {},
+        ["tags"] = {},
+        ["target"] = {},
+      })[f.name] == nil
+    end,
+  },
+  mappings = {
+    synchronize = "<cr>",
+    go_in = "",
+    go_in_plus = "l",
+  },
+}
