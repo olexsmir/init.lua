4c8e038fcbb4c85ac76b1071e7663bfae0da0e9b
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-14 16:19:56 +0200

    somehow i deleted those before

--- plugin/10_plugins.lua
+++ plugin/10_plugins.lua
@@ -1,3 +1,7 @@
+require("vim._core.ui2").enable { enable = true }
+vim.cmd.packadd "nvim.undotree"
+vim.cmd.packadd "nvim.difftool"
+
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
