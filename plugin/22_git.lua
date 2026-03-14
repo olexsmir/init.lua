3175a05266c08e0977cf062a185e99283cc7bcdd
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-14 14:30:46 +0200

    remove neogit

--- plugin/22_git.lua
+++ plugin/22_git.lua
@@ -29,38 +29,3 @@ Config.map("n", "<leader>gS", wrap "undo_stage_hunk")
 Config.map("n", "<leader>gr", wrap "reset_hunk")
 Config.map("n", "<leader>gh", wrap "preview_hunk")
 Config.map("n", "<leader>gb", wrap "blame")
-
--- neogit
-Config.later(function()
-  Config.add "TimUntersberger/neogit"
-  Config.map("n", "<leader>gg", vim.cmd.Neogit)
-  require("neogit").setup {
-    kind = "vsplit",
-    console_timeout = 8000,
-    auto_close_console = false,
-    disable_builtin_notifications = true,
-    disable_commit_confirmation = true,
-    disable_context_highlighting = true,
-    disable_hint = true,
-    auto_show_console = false,
-    graph_style = "kitty",
-    signs = {
-      section = { "", "" },
-      item = { "", "" },
-      hunk = { "", "" },
-    },
-    commit_editor = {
-      kind = "split",
-      staged_diff_split_kind = "split_above",
-    },
-    integrations = {
-      snacks = true,
-    },
-    sections = {
-      recent = {
-        folded = false,
-        hidden = false,
-      },
-    },
-  }
-end)
