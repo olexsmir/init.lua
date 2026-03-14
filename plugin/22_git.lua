7f9bd84bb5a4125908a22d07c6020c7a6a8a3806
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-14 11:59:35 +0200

    add some lazy loading

--- plugin/22_git.lua
+++ plugin/22_git.lua
@@ -1,7 +1,4 @@
-vim.pack.add {
-  "https://github.com/lewis6991/gitsigns.nvim",
-  "https://github.com/TimUntersberger/neogit",
-}
+vim.pack.add { "https://github.com/lewis6991/gitsigns.nvim" }
 
 require("gitsigns").setup {
   numhl = true,
@@ -35,34 +32,37 @@ Config.map("n", "<leader>gh", wrap "preview_hunk")
 Config.map("n", "<leader>gb", wrap "blame")
 
 -- neogit
+Config.later(function()
+  vim.pack.add { "https://github.com/TimUntersberger/neogit" }
 
-Config.map("n", "<leader>gg", vim.cmd.Neogit)
-require("neogit").setup {
-  kind = "vsplit",
-  console_timeout = 8000,
-  auto_close_console = false,
-  disable_builtin_notifications = true,
-  disable_commit_confirmation = true,
-  disable_context_highlighting = true,
-  disable_hint = true,
-  auto_show_console = false,
-  graph_style = "kitty",
-  signs = {
-    section = { "", "" },
-    item = { "", "" },
-    hunk = { "", "" },
-  },
-  commit_editor = {
-    kind = "split",
-    staged_diff_split_kind = "split_above",
-  },
-  integrations = {
-    snacks = true,
-  },
-  sections = {
-    recent = {
-      folded = false,
-      hidden = false,
+  Config.map("n", "<leader>gg", vim.cmd.Neogit)
+  require("neogit").setup {
+    kind = "vsplit",
+    console_timeout = 8000,
+    auto_close_console = false,
+    disable_builtin_notifications = true,
+    disable_commit_confirmation = true,
+    disable_context_highlighting = true,
+    disable_hint = true,
+    auto_show_console = false,
+    graph_style = "kitty",
+    signs = {
+      section = { "", "" },
+      item = { "", "" },
+      hunk = { "", "" },
     },
-  },
-}
+    commit_editor = {
+      kind = "split",
+      staged_diff_split_kind = "split_above",
+    },
+    integrations = {
+      snacks = true,
+    },
+    sections = {
+      recent = {
+        folded = false,
+        hidden = false,
+      },
+    },
+  }
+end)
