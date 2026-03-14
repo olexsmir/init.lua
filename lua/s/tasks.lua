9d948ae6c6a235f9a1567179f57681fb2ad569e9
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-14 16:10:45 +0200

    snip my things

--- lua/s/tasks.lua
+++ lua/s/tasks.lua
@@ -1,23 +1,7 @@
 local config = {
   label = "done:%Y%m%d-%H%M",
   archive_header = "# Archive",
-  tasks_file = vim.fn.stdpath "config" .. "/tasks.json",
 }
-
-local ns = vim.api.nvim_create_namespace "scratch.tasks"
-
----@return string[]
-local function get_tasks_files()
-  local f = io.open(config.tasks_file, "r")
-  if not f then error("cannot read " .. config.tasks_file) end
-  return vim.json.decode(f:read "*a")["files"] or error "'files' is not found"
-end
-
-local function display_filename(str)
-  local res = vim.fs.basename(str)
-  res = res:gsub("%.%w+$", "")
-  return res
-end
 
 ---@param str string
 local function is_task(str)
@@ -27,11 +11,6 @@ end
 ---@param str string
 local function is_task_labled(str)
   return str:match "^%s*%- %[[x ]%] `" ~= nil
-end
-
----@param str string
-local function has_next_tag(str)
-  return str:match "%#n[ext]*" ~= nil
 end
 
 ---@param str string
@@ -74,65 +53,6 @@ local function find_archive_heading(lines)
 end
 
 local tasks = {}
-function tasks.agenda()
-  local qf_items = vim
-    .iter(get_tasks_files())
-    :map(function(fname)
-      return vim
-        .iter(ipairs(vim.fn.readfile(fname)))
-        :filter(function(_, line)
-          return is_task(line) and has_next_tag(line)
-        end)
-        :map(function(lnum, line)
-          local task = remove_next_tag(line)
-          task = remove_task_link(task)
-          task = task:gsub("^%- %[ %] ", "") -- remove task prefix
-          task = task:gsub("`", "")
-
-          return {
-            lnum = lnum,
-            filename = fname,
-            text = task,
-            user_data = { filename = fname },
-          } --[[@as vim.quickfix.entry]]
-        end)
-        :totable()
-    end)
-    :flatten()
-    :totable()
-
-  vim.fn.setqflist({}, "r", {
-    nr = "$",
-    title = "scratch.tasks",
-    items = qf_items,
-    quickfixtextfunc = function(info)
-      local items = vim.fn.getqflist({ id = info.id, items = 1 }).items
-      local lines, highlights = {}, {}
-      for item = info.start_idx, info.end_idx do
-        local entry = items[item]
-        local fname = display_filename(entry.user_data.filename)
-        local fname_paded = fname .. string.rep(" ", 7 - #fname) .. " "
-
-        table.insert(lines, fname_paded .. entry.text)
-        table.insert(highlights, { fname_len = #fname_paded })
-      end
-
-      vim.schedule(function()
-        vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
-        for i, hl in ipairs(highlights) do
-          local line = info.start_idx + i - 2
-          vim.hl.range(0, ns, "qfFileName", { line, 0 }, { line, hl.fname_len })
-          vim.hl.range(0, ns, "Bold", { line, hl.fname_len }, { line, -1 })
-        end
-      end)
-
-      return lines
-    end,
-  })
-
-  vim.cmd.copen()
-end
-
 function tasks.complete()
   local bufnr = vim.api.nvim_get_current_buf()
   local task_idx = vim.api.nvim_win_get_cursor(0)[1]
