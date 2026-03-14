cdc5b7890f851112ea5e8a30a6493ede47e6b9f6
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-14 14:32:05 +0200

    separate mini and snacks

--- plugin/25_snacks.lua
+++ plugin/25_snacks.lua
@@ -1,21 +1,3 @@
-Config.add "echasnovski/mini.nvim"
-
-Config.map("n", "<leader>c", function()
-  require("mini.bufremove").delete()
-end)
-
-require("mini.icons").setup {}
-Config.later(function()
-  require("mini.ai").setup {}
-  require("mini.bufremove").setup { silent = true }
-  require("mini.comment").setup {}
-  require("mini.hipatterns").setup {}
-  require("mini.pairs").setup {}
-  require("mini.splitjoin").setup {}
-  require("mini.test").setup {}
-end)
-
--- snacks
 Config.add "folke/snacks.nvim"
 
 local function wrap(mod, fn, opts)
