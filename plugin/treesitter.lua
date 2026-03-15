f56ff6f7d31aa7fb83debd3938d7e36e4ee398b3
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-15 23:04:39 +0200

    no more numbers

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -7,7 +7,9 @@ Config.add "RRethy/nvim-treesitter-endwise"
 
 Config.later(function()
   Config.add "nvim-treesitter/nvim-treesitter-context"
-  require("treesitter-context").setup()
+  require("treesitter-context").setup {
+    max_lines = 4,
+  }
 end)
 
 Config.aucmd("FileType", "*", function()
