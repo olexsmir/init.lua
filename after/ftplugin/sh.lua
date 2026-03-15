51073cf365ae55a0b67ec9a27945a72e1a92d213
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-15 15:32:58 +0200

    i missed that

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,7 +1,5 @@
-local map = require("core.utils").map
-
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 
-map("n", "<localleader>x", ":!chmod +x %<CR><CR>", true)
+Config.map("n", "<localleader>x", ":!chmod +x %<CR><CR>", true)
