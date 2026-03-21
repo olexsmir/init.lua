fe4a8006b5bad200478340c9c553d2ccdb760c88
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-21 19:18:37 +0200

    now i do my notes differently, once again

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,9 +1,21 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.wrap = true
-vim.opt_local.list = false
+
 vim.opt_local.conceallevel = 2
-vim.opt_local.concealcursor = "cv"
+vim.opt_local.concealcursor = "c"
+Config.autocmd("InsertEnter", {
+  buffer = 0,
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+Config.autocmd("InsertLeave", {
+  buffer = 0,
+  callback = function()
+    vim.opt_local.conceallevel = 2
+  end,
+})
 
 Config.map("n", "<localleader>t", require("s.tasks").complete, true)
 Config.map("n", "<localleader>c", require("s.tasks").clear_archive, true)
@@ -12,12 +24,7 @@ Config.map("n", "<localleader>a", "<cmd>norm o- [ ] <cr>a", true)
 vim.b.minihipatterns_config = {
   highlighters = {
     next = { pattern = "%#n[ext]*", group = "MiniTestPass" },
-    front = { pattern = "front%:", group = "@boolean" },
-    feat = { pattern = "feat%:", group = "@boolean" },
-    chore = { pattern = "chore%:", group = "@character" },
-    refactor = { pattern = "refactor%:", group = "@comment.info" },
-    fix = { pattern = "fix%:", group = "@comment.error" },
-    docs = { pattern = "docs%:", group = "@label" },
-    test = { pattern = "test%:", group = "@comment.note" },
+    tags = { pattern = "^tags%:", group = "Boolean" },
+    links = { pattern = "https?://[^%s]+", group = "Tag" },
   },
 }
