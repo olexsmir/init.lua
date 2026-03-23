d8f1297cb42df9e32f11eeab61407f509cd643dc
Oleksandr Smirnov <olexsmir@gmail.com>
2026-03-23 14:39:51 +0200

    i do hacks

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -21,6 +21,7 @@ Config.later(function()
       fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
       todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
       note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+      hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
     },
   }
 end)
